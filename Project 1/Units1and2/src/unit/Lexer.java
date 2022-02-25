e4a49abcce34831c2af8a2e2a8acbd65df94e7db
tsw0015 <tsw0015@mix.wvu.edu>
2022-02-25 13:43:41 -0500

    updates

--- Project 1/Units1and2/src/unit/Lexer.java
+++ Project 1/Units1and2/src/unit/Lexer.java
@@ -11,7 +11,7 @@ public class Lexer extends model.AbstractLexer {
 		// TODO: implement this method stub
 		S = sentence;
 		i = 0;
-		throw new UnsupportedOperationException();
+		//throw new UnsupportedOperationException();
 	}
 
 	@Override
@@ -22,7 +22,7 @@ public class Lexer extends model.AbstractLexer {
 		if(S == null || i>= S.length)
 			return;
 
-		while(S[i] == ' ' || S[i] == '	') { 
+		while(S[i] == ' ' || S[i] == '	' || S[i] == '\n') { 
 			//TODO Account for all whitespace types
 			i++;
 			if ( i>= S.length)
@@ -66,22 +66,35 @@ public class Lexer extends model.AbstractLexer {
 			i++;
 		}
 		
-		//TODO Account for case insensitivity of keywords
-		else if(S[i] == 'l' || S[i] == 'e' || S[i] == 'v') {
-			if(i+1 < S.length && S[i+1] == 'e') {
+		//TODO Check for trailing characters that would cause the token to be variable name not keyword
+		//TODO Increment i correct number of space
+		//TODO handle mixed chases where var name begins with part of keyword but changes
+		else if(S[i] == 'l' || S[i] == 'L' ) {
+			if(i+1 < S.length && S[i+1] == 'e' || S[i+1] == 'E') {
 				if(i+2 < S.length && S[i+2] == 't') {
-
-				}
-			}
-			else if (i+1 < S.length && S[i+1] == 'v') {
-				if(i+2 < S.length && S[i+2] == 'a') {
-					if(i+3 < S.length && S[i+3] == 'l') {
-
+					if(i+3 < S.length && S[i+3] == ' ' || S[i+3] == '	' || S[i+3] == '\n') {
+						TOKEN = Token.LET_KEYWORD;
+						i+=3;
 					}
 				}
 			}
-			else if(i+1 < S.length && S[i+1] == ' ') {
-				
+		}
+		else if (S[i] == 'e' || S[i] == 'E') {
+			if (i+1 < S.length && S[i+1] == 'v' || S[i+1] == 'V') {
+				if(i+2 < S.length && S[i+2] == 'a' || S[i+1] == 'A') {
+					if(i+3 < S.length && S[i+3] == 'l' || S[i+1] == 'L') {
+						if(i+4 < S.length && S[i+4] == ' ' || S[i+4] == '	' || S[i+4] == '\n') {
+							TOKEN = Token.EVAL_KEYWORD;
+							i+=3;
+						}
+					}
+				}
+			}
+		}
+		else if(S[i] == 'v' || S[i] == 'V' ) {
+			if(i+1 < S.length && S[i+1] == ' ' || S[i+1] == '	' || S[i+1] == '\n') {
+				TOKEN = Token.VEE;
+				i++;
 			}
 		}
 		else if(S[i] >= 'a' && S[i] <= 'z') {
@@ -89,6 +102,6 @@ public class Lexer extends model.AbstractLexer {
 			i++;
 		}
 
-		throw new UnsupportedOperationException();
+		//throw new UnsupportedOperationException();
 	}
 }
