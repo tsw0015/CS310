83153c13079c7690407f477115b81965e9fe400e
tsw0015 <tsw0015@mix.wvu.edu>
2022-02-25 10:58:35 -0500

    Unit 1 Lexer updated to handle all single character lexemes

--- Project 1/Units1and2/src/unit/Lexer.java
+++ Project 1/Units1and2/src/unit/Lexer.java
@@ -3,16 +3,73 @@ package unit;
 public class Lexer extends model.AbstractLexer {
 	// TODO: do NOT redefine superclass fields
 
+	private char[] S;
+	private int i;
+
 	@Override
 	public void initialize(char[] sentence) {
 		// TODO: implement this method stub
-		
+		S = sentence;
+		i = 0;
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void lex() {
-		// TODO: implement this method stub
+		TOKEN = null;
+		LEXEME = null;
+
+		if(S == null || i>= S.length)
+			return;
+		
+		while(S[i] == ' ' || S[i] == '	') { 
+			//TODO Account for all whitespace types
+			i++;
+			if ( i>= S.length)
+				return;
+		}
+		
+		if(S[i] == '=') {
+			TOKEN = Token.EQUAL;
+			i++;
+		}
+		else if(S[i] == ',') {
+			TOKEN = Token.COMMA;
+			i++;
+		}
+		else if(S[i] == '?') {
+			TOKEN = Token.QUESTION;
+			i++;
+		}
+		else if(S[i] == 'v') {
+			TOKEN = Token.VEE;
+			i++;
+		}
+		else if(S[i] == '^') {
+			TOKEN = Token.CARET;
+			i++;
+		}
+		else if(S[i] == '\'') {
+			TOKEN = Token.APOSTROPHE;
+			i++;
+		}		
+		else if(S[i] == '(') {
+			TOKEN = Token.OPEN_PAREN;
+			i++;
+		}
+		else if(S[i] == ')') {
+			TOKEN = Token.CLOSE_PAREN;
+			i++;
+		}
+		else if(S[i] == '1') {
+			TOKEN = Token.TRUE_LITERAL;
+			i++;
+		}
+		else if(S[i] == '0') {
+			TOKEN = Token.FALSE_LITERAL;
+			i++;
+		}
+		
 		throw new UnsupportedOperationException();
 	}
 }
